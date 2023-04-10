ac7a6bf8d89eb234b3b0787c7c4d473c45db59d2
Olaxander <mittsykh@ukr.net>
2023-04-11 02:25:26 +0300

    Create groups list view

--- GroupManager/GroupManager/ViewModels/MainViewModel.cs
+++ GroupManager/GroupManager/ViewModels/MainViewModel.cs
@@ -10,13 +10,45 @@ using System.Windows;
 
 namespace GroupManager.ViewModels
 {
-    public class MainViewModel:PropertyChangedBase
+    public class MainViewModel:Screen
     {
         IRepository<Group> _groupRepository;
+        BindableCollection<Group> _groups { get; set; }
+        public BindableCollection<Group> Groups
+        {
+            get => _groups;
+            set
+            {
+                _groups= value;
+                NotifyOfPropertyChange(() => Groups);
+            }
+        }
         public MainViewModel(
             IRepository<Group> repository)
         {
             _groupRepository = repository;
+            //UploadGroups();
+        }
+        private async void UploadGroups()
+        {
+            Groups=new BindableCollection<Group>(await _groupRepository.GetAllAsync());
+        }
+
+        public void AddGroup()
+        {
+            Group newGroup = new Group
+            {
+                Id=Guid.NewGuid(),
+                Name = "Naew",
+            };
+            _groupRepository.Add(newGroup);
+        }
+
+        protected override async void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            Groups = new BindableCollection<Group>(await _groupRepository.GetAllAsync());
+            Groups.Add(new Group { Name = "PV-02" });
         }
 
     }
